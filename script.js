8ec43667a864e8b9b2f4e6cfdf6b443a637cc788
winta-git <78151648+winta-git@users.noreply.github.com>
2021-02-06 16:55:28 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function showTemperature(response) {
+    let temperatureElement = document.querySelector(⋕temeperature);
+    let cityElement = document.querySelector(⋕city);
+    let descriptionElement = document.querySelector(⋕description);
+    temperatureElemtnt.InnerHTML = math.round(response.data.main.temp);
+    cityElement = response.data.name;
+    descriptionElement.InnerHTML = response.data.weather[0].description.;
+}
+let apikey = "db19e864f86e1792fa09a7dafc23c7cf";
+let apiurl = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}";
+
+let h1 = document.querySelector(⋕ city);
+
+Axios.get(apiurl).then(show temperature);
