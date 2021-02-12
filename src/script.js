786e7d8432d65df53c349a9791b38cde302d4c4f
winta-git <78151648+winta-git@users.noreply.github.com>
2021-02-12 11:01:32 +0000

    Added to style

--- src/script.js
+++ src/script.js
@@ -1,18 +1,18 @@
-function showTemperature(response) {
+function displayTemperature(response) {
     let temperatureElement = document.querySelector("⋕temperature");
     let cityElement = document.querySelector("⋕city");
     let descriptionElement = document.querySelector("⋕description");
     let humidityElement = document.querySelector("⋕himudity");
     let windElement = document.querySelector("⋕wind");
-    temperatureElemtnt.InnerHTML = math.round("response.data.main.temp");
+    temperatureElement.InnerHTML = math.round(response.data.main.temp);
     cityElement = response.data.name;
     descriptionElement.InnerHTML = response.data.weather[0].description;
-    himudityElement.InnerHTML = response.data.main.himudity;
+    himidityElement.InnerHTML = response.data.main.himidity;
     windElement.InnerHTML = math.round(respond.data.wind.speed);
 }
 let apikey = "db19e864f86e1792fa09a7dafc23c7cf";
-let apiurl = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}";
+let apiurl = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=db19e864f86e1792fa09a7dafc23c7cf&units=metric';
 
 let h1 = document.querySelector("⋕city");
 
-Axios.get(apiurl).then("showTemperature");
+axios.get(apiurl).then(displayTemperature);
