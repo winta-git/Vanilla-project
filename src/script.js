f60adb77ad60e5c9b4b2e6fb3a4892d6b7024bc8
winta-git <78151648+winta-git@users.noreply.github.com>
2021-02-13 00:51:01 +0000

    Update date

--- src/script.js
+++ src/script.js
@@ -1,17 +1,34 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = '0${minutes}';
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = '0s{minutes}';
+    }
+}
+let days = ["Sunday","Monday","Tuesday","wednesday","thursday","Friday","Saturday"]
+let day = days[date.getday()];
+return '${day} ${hours}:${minutes}';
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("⋕temperature");
     let cityElement = document.querySelector("⋕city");
     let descriptionElement = document.querySelector("⋕description");
     let humidityElement = document.querySelector("⋕himudity");
-    let windElement = document.querySelector("⋕wind");
-    temperatureElement.InnerHTML = math.round(response.data.main.temp);
+    let windElement = document.querySelector ("⋕wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement = response.data.name;
-    descriptionElement.InnerHTML = response.data.weather[0].description;
-    himidityElement.InnerHTML = response.data.main.himidity;
-    windElement.InnerHTML = math.round(respond.data.wind.speed);
+    descriptionElement.innerHTML= response.data.weather[0].description;
+    himidityElement.innerHTML = response.data.main.himidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    HTMLTableDataCellElement.innerHTML = formatDate(response.date.dt * 1000);
 }
-let apikey = "db19e864f86e1792fa09a7dafc23c7cf";
-let apiurl = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=db19e864f86e1792fa09a7dafc23c7cf&units=metric';
+
+let apikey = "eac809de9b5da86f7dca73c1830c1d4c";
+let apiurl = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=eac809de9b5da86f7dca73c1830c1d4c';
 
 let h1 = document.querySelector("⋕city");
 
