703b2634286c675d08a89ade1aadd80179037a9c
winta-git <78151648+winta-git@users.noreply.github.com>
2021-02-13 16:03:51 +0000

    Create a link for celsius

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 function formatDate(timestamp) {
+  
     let date = new Date(timestamp);
     let hours = date.getHours();
     if (hours < 10) {
@@ -8,11 +9,13 @@ function formatDate(timestamp) {
     if (minutes < 10) {
         minutes = '0s{minutes}';
     }
-}
+
 let days = ["Sunday","Monday","Tuesday","wednesday","thursday","Friday","Saturday"]
-let day = days[date.getday()];
+let day = days[date.getday(0)];
 return `${day} ${hours}:${minutes}`;
 
+
+}
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("⋕temperature");
     let cityElement = document.querySelector("⋕city");
@@ -21,7 +24,9 @@ function displayTemperature(response) {
     let windElement = document.querySelector("⋕wind");
     let iconElement = document.querySelector("⋕icon");
 
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    celsiusTemperature = response.data.main.temp;
+
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement = response.data.name;
     descriptionElement.innerHTML= response.data.weather[0].description;
     himidityElement.innerHTML = response.data.main.himidity;
@@ -32,8 +37,9 @@ function displayTemperature(response) {
 }
 function search(city) {
 let apikey = "eac809de9b5da86f7dca73c1830c1d4c";
-let city = London
-let apiurl = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=eac809de9b5da86f7dca73c1830c1d4c';
+    let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=eac809de9b5da86f7dca73c1830c1d4c&units=metric';
+    axios.get(apiUrl).then(displayTemperature);
+
 }
 function handleSubmit(event) {
     event.preventDefault();
@@ -41,19 +47,29 @@ function handleSubmit(event) {
     search(cityInputElement.value);
     console.log(cityInputElement.value);
 }
-function showFahrenheitTemperature(event) {
+function displayFahrenheitTemperature(event) {
     event.preventDefault();
-    let fahrenheitTemperature = (25 * 9) / 5 + 32;
+    let fahrenheitTemperature = (celsiusTemperatur * 9) / 5 + 32;
     alert("Link clicked");
+    let temperatureElement = document.querySelector("⋕temperature");
+    temperatureElement.innerHTML = Math.round (fahrenheitTemperature);
+
 
 }
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("⋕temperature");
+    temperatureElement.innerHTML = celsiusTemperature;
+}
+let celsiusTemperature = null;
 
-search("London");
 
-let h1 = document.querySelector("⋕city");
-
-axios.get(apiurl).then(displayTemperature);
 let form = document.querySelector("search-form");
 form.addEventListener("submit", handleSubmit);
+
+
 let fahrenheitlink = document.querySelector("⋕fehrenheit-link")
-fahrenheitlink.addEventListener("click", showFahrenheitTemperature);
+fahrenheitlink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiuslink = document.querySelector("⋕celsius-link")
+celsiuslink.addEventListener("click", displayCelsiusTemperature);
